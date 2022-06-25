4e82b40e53508a38e46edee0962aa242e9650f6a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-06-25 22:43:21 +0530

    Create Alan.jsx

---
+++ src/components/Alan.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useContext } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import { fetchToken, logoutUser } from '../utils';
+import { ColorModeContext } from '../utils/ToggleColorMode';
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_ALAN_AI_KEY,
+      onCommand: ({ command, mode }) => {
+        if (command === 'changeMode') {
+          if (mode === 'dark') {
+            setMode('dark');
+          } else {
+            setMode('light');
+          }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          logoutUser();
+          window.location.href = '/';
+        }
+      },
+    });
+  }, []);
+};
+
+export default useAlan;
