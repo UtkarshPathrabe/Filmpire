425df63dc46451d15a77a6d1c0d2e563935b57f9
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-06-25 22:43:36 +0530

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ACCOUNT_ID, REQUEST_TOKEN, SESSION_ID } from '../constants';
 
 export const moviesApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
@@ -12,7 +13,7 @@ export const fetchToken = async () => {
     const { data } = await moviesApi.get('/authentication/token/new');
     const token = data.request_token;
     if (data.success) {
-      localStorage.setItem('request_token', token);
+      localStorage.setItem(REQUEST_TOKEN, token);
       window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=${window.location.origin}/approved`;
     }
   } catch (error) {
@@ -21,12 +22,12 @@ export const fetchToken = async () => {
 };
 
 export const createSeesionId = async () => {
-  const token = localStorage.getItem('request_token');
+  const token = localStorage.getItem(REQUEST_TOKEN);
   if (token) {
     try {
       const { data: { session_id } } = await moviesApi.post('/authentication/session/new', { request_token: token });
       if (session_id) {
-        localStorage.setItem('session_id', session_id);
+        localStorage.setItem(SESSION_ID, session_id);
       }
       return session_id;
     } catch (error) {
@@ -34,4 +35,10 @@ export const createSeesionId = async () => {
     }
   }
   return undefined;
+};
+
+export const logoutUser = () => {
+  localStorage.removeItem(SESSION_ID);
+  localStorage.removeItem(ACCOUNT_ID);
+  localStorage.removeItem(REQUEST_TOKEN);
 };
