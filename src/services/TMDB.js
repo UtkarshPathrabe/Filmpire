73c190fc9251564ee0cb4ef139f513de59f31842
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-06-25 20:50:50 +0530

    Update TMDB.js

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -34,8 +34,11 @@ export const tmdbApi = createApi({
       query: (id) => `movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
     }),
     //* Get User Specific List
+    getList: builder.query({
+      query: ({ listName, accountId, sessionId, page }) => `account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`,
+    }),
     getRecommendations: builder.query({
-      query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
+      query: ({ movie_id, list }) => `movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
     }),
     //* Get Actor
     getActorDetails: builder.query({
@@ -51,6 +54,7 @@ export const {
   useGetGenresQuery,
   useGetMoviesQuery,
   useGetMovieQuery,
+  useGetListQuery,
   useGetRecommendationsQuery,
   useGetActorDetailsQuery,
   useGetMoviesByActorIdQuery,
