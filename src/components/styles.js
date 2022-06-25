9c585a9f4fd26896e9eaf359a0739872a053ae85
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-06-25 18:34:51 +0530

    Update styles.js

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@mui/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   root: {
     display: 'flex',
     height: '100%',
@@ -11,5 +11,11 @@ export default makeStyles(() => ({
   content: {
     flexGrow: '1',
     padding: '2em',
+    [theme.breakpoints.down('md')]: {
+      padding: '2em 0.5em',
+    },
+    [theme.breakpoints.down('sm')]: {
+      padding: '2em 0.25em',
+    },
   },
 }));
