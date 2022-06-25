e90ea8483bc2372d9e9eb2eac42bf716a703715a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-06-25 20:50:46 +0530

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,15 +3,25 @@ import { useSelector } from 'react-redux';
 import { Box, Typography, Button } from '@mui/material';
 import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
+import { useGetListQuery } from '../../services/TMDB';
+import { RatedCards } from '..';
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
-  const favouriteMovies = [];
+  const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({ listName: 'favorite/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+  const { data: watchlistMovies, refetch: refetchWatchlisted } = useGetListQuery({ listName: 'watchlist/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+
+  useEffect(() => {
+    refetchFavorites();
+    refetchWatchlisted();
+  }, []);
 
   const logout = () => {
     localStorage.clear();
     window.location.href = '/';
   };
+
+  console.log('profile', favoriteMovies, watchlistMovies);
 
   return (
     <Box>
@@ -21,11 +31,12 @@ const Profile = () => {
           Logout &nbsp; <ExitToApp />
         </Button>
       </Box>
-      { !favouriteMovies.length
+      { !favoriteMovies?.results?.length && !watchlistMovies?.results?.length
         ? <Typography>Add favourites or watchlist some movies to see them here!</Typography>
         : (
           <Box>
-            Favourite Movies
+            <RatedCards title="Favorite Movies" data={favoriteMovies} />
+            <RatedCards title="Watchlist" data={watchlistMovies} />
           </Box>
         ) }
     </Box>
