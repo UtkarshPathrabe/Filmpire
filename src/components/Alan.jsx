7d88a73fb1fbe11831e4211ca036258d6aa4bae5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-06-26 11:23:14 +0530

    Update Alan.jsx

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,25 +1,37 @@
 import { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
-
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { fetchToken, logoutUser } from '../utils';
 import { ColorModeContext } from '../utils/ToggleColorMode';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
   useEffect(() => {
     alanBtn({
       key: process.env.REACT_APP_ALAN_AI_KEY,
-      onCommand: ({ command, mode }) => {
-        if (command === 'changeMode') {
-          if (mode === 'dark') {
-            setMode('dark');
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const genreFound = genres.find((g) => (g.name.toLowerCase() === genreOrCategory.toLowerCase()));
+          if (genreFound) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(genreFound.id));
           } else {
-            setMode('light');
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
           }
+        } else if (command === 'changeMode') {
+          setMode((mode === 'dark') ? 'dark' : 'light');
         } else if (command === 'login') {
           fetchToken();
         } else if (command === 'logout') {
           logoutUser();
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
         }
       },
     });
