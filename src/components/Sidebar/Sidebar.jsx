0f36552af12c75c32efbcbb603a897a7544bf551
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-06-26 14:07:26 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,15 +8,13 @@ import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetGenresQuery } from '../../services/TMDB';
 import genreIcons from '../../assets/genres';
 import useStyles from './styles';
+import { BLUE_LOGO, RED_LOGO } from '../../constants';
 
 const categories = [
   { label: 'Popular', value: 'popular' },
   { label: 'Top Rated', value: 'top_rated' },
   { label: 'Upcoming', value: 'upcoming' },
 ];
-
-const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
-const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
 const Sidebar = ({ setMobileOpen }) => {
   const theme = useTheme();
@@ -32,7 +30,7 @@ const Sidebar = ({ setMobileOpen }) => {
   return (
     <>
       <Link to="/" className={classes.imageLink}>
-        <img className={classes.image} src={theme.palette.mode === 'light' ? redLogo : blueLogo} alt="Filmpire logo" />
+        <img className={classes.image} src={theme.palette.mode === 'light' ? BLUE_LOGO : RED_LOGO} alt="Filmpire logo" />
       </Link>
       <Divider />
       <List>
